8ddf13d94ad9be1c148fe8fbbe1ad4d2d39f7dd3
Erick Muñoz <musal1985@gmail.com>
2025-01-05 11:09:30 -0400

    publication detail done

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PublicationListView
+from .views import PublicationListView, PublicationDetailView
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publication-list'),
+    path("publication/<int:pk>", PublicationDetailView.as_view(), name="publication-detail")
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Publication
 
 # Create your views here.
 class PublicationListView(ListView):
     model = Publication
     template_name = "publications-list.html"
+
+class PublicationDetailView(DetailView):
+    model = Publication
+    template_name = "publication-detail.html" 
